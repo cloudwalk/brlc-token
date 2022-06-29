9c37fe9e573515374eb6cf5f84b498130e28e6d3
IgorSenychCW <igor.senych@cloudwalk.io>
2022-06-29 16:03:36 +0300

    Update SubstrateBRLCTokenV3Upgradeable contract.
    Move bridging functionality to the separate contract BridgeableTokenUpgradeable.

--- contracts/tokens/SubstrateBRLCTokenV3Upgradeable.sol
+++ contracts/tokens/SubstrateBRLCTokenV3Upgradeable.sol
@@ -2,112 +2,45 @@
 pragma solidity >=0.6.0 <0.8.0;
 
 import {SafeMathUpgradeable} from "@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol";
-import {IERC20Bridgeable} from "../base/interfaces/IERC20Bridgeable.sol";
 import {SubstrateBRLCTokenV2Upgradeable} from "./SubstrateBRLCTokenV2Upgradeable.sol";
+import {BridgeableTokenUpgradeable} from "./core/BridgeableTokenUpgradeable.sol";
 
 /**
  * @title SubstrateBRLCTokenV3Upgradeable contract
  * @dev V3 changes:
- * - Added bridging functionality.
+ * - Added bridging functionality
  */
 contract SubstrateBRLCTokenV3Upgradeable is
     SubstrateBRLCTokenV2Upgradeable,
-    IERC20Bridgeable
+    BridgeableTokenUpgradeable
 {
-    address private _bridge;
-
-    event BridgeChanged(address indexed newBridge);
-
     function initialize(
         string memory name_,
         string memory symbol_,
         uint8 decimals_
     ) public virtual override initializer {
-        __SubstrateBRLCTokenUpgradeable_init(name_, symbol_, decimals_);
-        __SubstrateBRLCTokenV2Upgradeable_init_unchained();
-        __SubstrateBRLCTokenV3Upgradeable_init_unchained();
+        __SubstrateBRLCTokenV3_init(name_, symbol_, decimals_);
     }
 
-    function __SubstrateBRLCTokenV3Upgradeable_init_unchained()
-        internal
-        initializer
-    {}
-
-    /**
-     * @dev Throws if called by any account other than the bridge.
-     */
-    modifier onlyBridge() {
-        require(
-            _msgSender() == bridge(),
-            "Bridgeable: caller is not the bridge"
-        );
-        _;
+    function __SubstrateBRLCTokenV3_init(
+        string memory name_,
+        string memory symbol_,
+        uint8 decimals_
+    ) internal initializer {
+        __Context_init_unchained();
+        __Ownable_init_unchained();
+        __Rescuable_init_unchained();
+        __Pausable_init_unchained();
+        __PausableEx_init_unchained();
+        __Blacklistable_init_unchained();
+        __ERC20_init_unchained(name_, symbol_);
+        __BaseToken_init_unchained(decimals_);
+        __SubstrateBRLCToken_init_unchained();
+        __MintableToken_init_unchained();
+        __SubstrateBRLCTokenV2_init_unchained();
+        __BridgeableToken_init_unchained();
+        __SubstrateBRLCTokenV3_init_unchained();
     }
 
-    /**
-     * @dev Mints tokens as part of a bridge operation.
-     * @param account The owner of the tokens passing through the bridge.
-     * @param amount The amount of tokens passing through the bridge.
-     * @return True if the operation was successful.
-     */
-    function mintForBridging(address account, uint256 amount)
-        external
-        override
-        onlyBridge
-        returns (bool)
-    {
-        require(
-            account != address(0),
-            "Bridgeable: minting for the zero address"
-        );
-        require(
-            amount > 0,
-            "Bridgeable: minting amount is not greater than 0"
-        );
-
-        _mint(account, amount);
-        emit MintForBridging(account, amount);
-
-        return true;
-    }
-
-    /**
-     * @dev Burns tokens as part of a bridge operation.
-     * @param account The owner of the tokens passing through the bridge.
-     * @param amount The amount of tokens passing through the bridge.
-     * @return True if the operation was successful.
-     */
-    function burnForBridging(address account, uint256 amount)
-        external
-        override
-        onlyBridge
-        returns (bool)
-    {
-        require(amount > 0, "Bridgeable: burning amount is not greater than 0");
-        require(
-            balanceOf(_msgSender()) >= amount,
-            "Bridgeable: burning amount exceeds the bridge balance"
-        );
-
-        _burn(_msgSender(), amount);
-        emit BurnForBridging(account, amount);
-
-        return true;
-    }
-
-    /**
-     * @dev Returns the address of the bridge.
-     */
-    function bridge() public view override returns (address) {
-        return _bridge;
-    }
-
-    /**
-     * @dev Sets the address of the bridge.
-     * @param newBridge The address of the new bridge.
-     */
-    function setBridge(address newBridge) external onlyOwner {
-        _bridge = newBridge;
-        emit BridgeChanged(newBridge);
-    }
+    function __SubstrateBRLCTokenV3_init_unchained() internal initializer {}
 }
