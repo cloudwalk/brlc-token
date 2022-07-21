d811a7691bfbaf26a440a71f5b32689d6827e43c
Evgenii Zaitsev <evgenii@cloudwalk.io>
2022-07-21 20:55:30 +0700

    tests: develop tests for the CPP contract

--- test-utils/misc.ts
+++ test-utils/misc.ts
@@ -3,3 +3,24 @@ export function countNumberArrayTotal(array: number[]) {
     return sum + currentValue;
   });
 }
+
+export function createBytesString(baseString: string | number | undefined, byteLength: number) {
+  baseString = !baseString ? '' : baseString.toString();
+  if (baseString.length > byteLength * 2) {
+    throw new Error(
+      `Creating of bytes string failed. `
+      + `The length of the base string if greater than allowed maximum length. `
+      + `The base string: '${baseString}'`
+      + `The target byte length: '${byteLength}'`
+    );
+  }
+  if (!/^[0-9a-fA-F]+$/.test(baseString)) {
+    throw new Error(
+      `Creating of bytes16 string failed. `
+      + `The base string content is incorrect `
+      + `The base string: '${baseString}'`
+    );
+  }
+
+  return '0x' + '0'.repeat(byteLength * 2 - baseString.length) + baseString.toLowerCase();
+}
