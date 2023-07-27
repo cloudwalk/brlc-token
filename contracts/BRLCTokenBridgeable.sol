83f16a213f78a75a95ee503fa33a04adc19f029a
igorsenych-cw <igor.senych@cloudwalk.io>
2023-07-27 14:57:35 +0300

    Update BRLCTokenBridgeable contract by inheriting base contracts

--- contracts/BRLCTokenBridgeable.sol
+++ contracts/BRLCTokenBridgeable.sol
@@ -2,60 +2,16 @@
 
 pragma solidity 0.8.16;
 
-import { IERC20Bridgeable } from "./base/interfaces/IERC20Bridgeable.sol";
-import { IERC20Freezable } from "./base/interfaces/IERC20Freezable.sol";
 import { ERC20Base } from "./base/ERC20Base.sol";
+import { ERC20Bridgeable } from "./base/ERC20Bridgeable.sol";
+import { ERC20Freezable } from "./base/ERC20Freezable.sol";
 
 /**
  * @title BRLCTokenBridgeable contract
  * @author CloudWalk Inc.
  * @dev The BRLC token implementation that supports the bridge operations.
  */
-contract BRLCTokenBridgeable is ERC20Base, IERC20Bridgeable, IERC20Freezable {
-    /// @dev The address of the bridge.
-    address private _bridge;
-
-    /// @dev The mapping of the freeze approvals.
-    mapping(address => bool) private _freezeApprovals;
-
-    /// @dev The mapping of the frozen balances.
-    mapping(address => uint256) private _frozenBalances;
-
-    // -------------------- Errors -----------------------------------
-
-    /// @dev The transaction sender is not a bridge.
-    error UnauthorizedBridge(address account);
-
-    /// @dev The zero amount of tokens is passed during the mint operation.
-    error ZeroMintForBridgingAmount();
-
-    /// @dev The zero amount of tokens is passed during the burn operation.
-    error ZeroBurnForBridgingAmount();
-
-    /// @dev The token freezing operation is not approved by the account.
-    error FreezingNotApproved();
-
-    /// @dev The token freezing is already approved by the account.
-    error FreezingAlreadyApproved();
-
-    /// @dev The frozen balance is exceeded during the operation.
-    error LackOfFrozenBalance();
-
-    /// @dev The transfer amount exceeded the frozen amount.
-    error TransferExceededFrozenAmount();
-
-    // -------------------- Modifiers --------------------------------
-
-    /// @dev Throws if called by any account other than the bridge.
-    modifier onlyBridge() {
-        if (_msgSender() != _bridge) {
-            revert UnauthorizedBridge(_msgSender());
-        }
-        _;
-    }
-
-    // -------------------- Functions --------------------------------
-
+contract BRLCTokenBridgeable is ERC20Base, ERC20Bridgeable, ERC20Freezable {
     /**
      * @dev Constructor that prohibits the initialization of the implementation of the upgradable contract.
      *
@@ -81,11 +37,7 @@ contract BRLCTokenBridgeable is ERC20Base, IERC20Bridgeable, IERC20Freezable {
      * @param symbol_ The symbol of the token.
      * @param bridge_ The address of a bridge contract to support by this contract.
      */
-    function initialize(
-        string memory name_,
-        string memory symbol_,
-        address bridge_
-    ) external virtual initializer {
+    function initialize(string memory name_, string memory symbol_, address bridge_) external virtual initializer {
         __BRLCTokenBridgeable_init(name_, symbol_, bridge_);
     }
 
@@ -106,7 +58,9 @@ contract BRLCTokenBridgeable is ERC20Base, IERC20Bridgeable, IERC20Freezable {
         __Blacklistable_init_unchained();
         __ERC20_init_unchained(name_, symbol_);
         __ERC20Base_init_unchained();
-        __BRLCTokenBridgeable_init_unchained(bridge_);
+        __ERC20Bridgeable_init_unchained(bridge_);
+        __ERC20Freezable_init_unchained();
+        __BRLCTokenBridgeable_init_unchained();
     }
 
     /**
@@ -114,162 +68,23 @@ contract BRLCTokenBridgeable is ERC20Base, IERC20Bridgeable, IERC20Freezable {
      *
      * See {BRLCTokenBridgeable-initialize}.
      */
-    function __BRLCTokenBridgeable_init_unchained(address bridge_) internal onlyInitializing {
-        _setBridge(bridge_);
-    }
+    function __BRLCTokenBridgeable_init_unchained() internal onlyInitializing {}
 
     /**
-     * @dev See {IERC20Bridgeable-setBridge}.
+     * @dev Returns true if token is BRLCoin implementation.
      */
-    function setBridge(address newBridge) external onlyOwner {
-        _setBridge(newBridge);
-    }
-
-    /**
-     * @dev See {IERC20Bridgeable-mintForBridging}.
-     *
-     * Requirements:
-     *
-     * - Can only be called by the bridge.
-     * - The `amount` value must be greater than zero.
-     */
-    function mintForBridging(address account, uint256 amount) external onlyBridge returns (bool) {
-        if (amount == 0) {
-            revert ZeroMintForBridgingAmount();
-        }
-
-        _mint(account, amount);
-        emit MintForBridging(account, amount);
-
+    function isBRLCoin() external pure returns (bool) {
         return true;
     }
 
     /**
-     * @dev See {IERC20Bridgeable-burnForBridging}.
-     *
-     * Requirements:
-     *
-     * - Can only be called by the bridge.
-     * - The `amount` value must be greater than zero.
+     * @dev See {ERC20Upgradeable-_beforeTokenTransfer}.
      */
-    function burnForBridging(address account, uint256 amount) external onlyBridge returns (bool) {
-        if (amount == 0) {
-            revert ZeroBurnForBridgingAmount();
-        }
-
-        _burn(account, amount);
-        emit BurnForBridging(account, amount);
-
-        return true;
-    }
-
-    /**
-     * @dev See {IERC20Freezable-approveFreezing}.
-     *
-     * Requirements:
-     *
-     * - The contract must not be paused.
-     */
-    function approveFreezing() whenNotPaused external {
-        if (_freezeApprovals[_msgSender()]) {
-            revert FreezingAlreadyApproved();
-        }
-
-        _freezeApprovals[_msgSender()] = true;
-
-        emit FreezeApproval(_msgSender());
-    }
-
-    /**
-     * @dev See {IERC20Freezable-freeze}.
-     *
-     * Requirements:
-     *
-     * - The contract must not be paused.
-     * - Can only be called by the blacklister account.
-     * - The token freezing must be approved by the `account`.
-     */
-    function freeze(address account, uint256 amount) external whenNotPaused onlyBlacklister {
-        if(!_freezeApprovals[account]) {
-            revert FreezingNotApproved();
-        }
-
-        emit Freeze(account, amount, _frozenBalances[account]);
-
-        _frozenBalances[account] = amount;
-    }
-
-    /**
-     * @dev See {IERC20Freezable-transferFrozen}.
-     *
-     * Requirements:
-     *
-     * - The contract must not be paused.
-     * - Can only be called by the blacklister account.
-     * - The frozen balance must be greater than the `amount`.
-     */
-    function transferFrozen(address from, address to, uint256 amount) public virtual whenNotPaused onlyBlacklister {
-        uint256 balance = _frozenBalances[from];
-
-        if(amount > balance) {
-            revert LackOfFrozenBalance();
-        }
-
-        unchecked {
-            _frozenBalances[from] -= amount;
-        }
-
-        emit FreezeTransfer(from, amount);
-        emit Freeze(from, _frozenBalances[from], balance);
-
-        _transfer(from, to, amount);
-    }
-
-    /**
-     * @dev See {IERC20Bridgeable-isBridgeSupported}.
-     */
-    function isBridgeSupported(address bridge_) external view returns (bool) {
-        return _bridge == bridge_;
-    }
-
-    /// @dev Returns the bridge address.
-    function bridge() external view virtual returns (address) {
-        return _bridge;
-    }
-
-    /**
-     * @dev See {IERC20Bridgeable-isIERC20Bridgeable}.
-     */
-    function isIERC20Bridgeable() external pure returns (bool) {
-        return true;
-    }
-
-    /**
-     * @dev See {IERC20Freezable-freezeApproval}.
-     */
-    function freezeApproval(address account) external view returns (bool) {
-        return _freezeApprovals[account];
-    }
-
-    /**
-     * @dev See {IERC20Freezable-frozenBalance}.
-     */
-    function frozenBalance(address account) external view returns (uint256) {
-        return _frozenBalances[account];
-    }
-
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override {
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override(ERC20Base, ERC20Freezable) {
         super._beforeTokenTransfer(from, to, amount);
-        uint256 frozen = _frozenBalances[from];
-        if (frozen != 0) {
-            if(balanceOf(from) < frozen + amount) {
-                revert TransferExceededFrozenAmount();
-            }
-        }
-    }
-
-    function _setBridge(address newBridge) internal {
-        emit SetBridge(newBridge, _bridge);
-        _bridge = newBridge;
     }
 }
--- test/BRLCTokenBridgeable.test.ts
+++ test/BRLCTokenBridgeable.test.ts
@@ -17,344 +17,60 @@ describe("Contract 'BRLCTokenBridgeable'", async () => {
   const TOKEN_NAME = "BRL Coin";
   const TOKEN_SYMBOL = "BRLC";
   const TOKEN_DECIMALS = 6;
-  const TOKEN_AMOUNT = 123;
 
-  const EVENT_NAME_BURN_FOR_BRIDGING = "BurnForBridging";
-  const EVENT_NAME_MINT_FOR_BRIDGING = "MintForBridging";
-  const EVENT_NAME_SET_BRIDGE = "SetBridge";
-  const EVENT_NAME_FREEZE_APPROVAL = "FreezeApproval";
-  const EVENT_NAME_FREEZE = "Freeze";
-  const EVENT_NAME_FREEZE_TRANSFER = "FreezeTransfer";
+  const REVERT_MESSAGE_INITIALIZABLE_CONTRACT_IS_ALREADY_INITIALIZED = "Initializable: contract is already initialized";
 
-  const REVERT_MESSAGE_IF_CONTRACT_IS_ALREADY_INITIALIZED = "Initializable: contract is already initialized";
-  const REVERT_MESSAGE_IF_CALLER_IS_NOT_OWNER = "Ownable: caller is not the owner";
-  const REVERT_MESSAGE_IF_BURNING_AMOUNT_EXCEEDS_THE_BRIDGE_BALANCE = "ERC20: burn amount exceeds balance";
-  const REVERT_MESSAGE_IF_TRANSFER_AMOUNT_EXCEEDS_BALANCE = "ERC20: transfer amount exceeds balance";
-  const REVERT_MESSAGE_IF_BURNING_FROM_ZERO_ADDRESS = "ERC20: burn from the zero address";
-  const REVERT_MESSAGE_IF_MINTING_FOR_ZERO_ADDRESS = "ERC20: mint to the zero address";
-  const REVERT_MESSAGE_IF_CONTRACT_IS_PAUSED = "Pausable: paused";
-
-  const REVERT_ERROR_IF_BURN_FOR_BRIDGING_AMOUNT_IS_ZERO = "ZeroBurnForBridgingAmount";
-  const REVERT_ERROR_IF_CALLER_IS_NOT_BRIDGE = "UnauthorizedBridge";
-  const REVERT_ERROR_IF_MINT_FOR_BRIDGING_AMOUNT_IS_ZERO = "ZeroMintForBridgingAmount";
-  const REVERT_ERROR_IF_CALLER_IS_NOT_BLACKLISTER = "UnauthorizedBlacklister";
-  const REVERT_ERROR_IF_FREEZING_NOT_APPROVED = "FreezingNotApproved";
-  const REVERT_ERROR_IF_FREEZING_ALREADY_APPROVED = "FreezingAlreadyApproved";
-  const REVERT_ERROR_IF_LACK_OF_FROZEN_BALANCE = "LackOfFrozenBalance";
-  const REVERT_ERROR_IF_TRANSFER_EXCEEDED_FROZEN_AMOUNT = "TransferExceededFrozenAmount";
-
-
-  let brlcTokenFactory: ContractFactory;
+  let tokenFactory: ContractFactory;
   let deployer: SignerWithAddress;
-  let blacklister: SignerWithAddress;
-  let pauser: SignerWithAddress;
-  let user1: SignerWithAddress;
-  let user2: SignerWithAddress;
   let bridge: SignerWithAddress;
-  let newBridge: SignerWithAddress;
 
   before(async () => {
-    [deployer, blacklister, pauser, user1, user2, bridge, newBridge] = await ethers.getSigners();
-    brlcTokenFactory = await ethers.getContractFactory("BRLCTokenBridgeable");
+    [deployer, bridge] = await ethers.getSigners();
+    tokenFactory = await ethers.getContractFactory("BRLCTokenBridgeable");
   });
 
-  async function deployBrlcToken(): Promise<{ brlcToken: Contract }> {
-    const brlcToken: Contract = await upgrades.deployProxy(
-      brlcTokenFactory,
+  async function deployToken(): Promise<{ token: Contract }> {
+    const token: Contract = await upgrades.deployProxy(
+      tokenFactory,
       [TOKEN_NAME, TOKEN_SYMBOL, bridge.address]
     );
-    await brlcToken.deployed();
-    return { brlcToken };
-  }
-
-  async function deployAndConfigureBrlcToken(): Promise<{ brlcToken: Contract }> {
-    const { brlcToken } = await deployBrlcToken();
-    await proveTx(brlcToken.setPauser(pauser.address));
-    await proveTx(brlcToken.setBlacklister(blacklister.address));
-    return { brlcToken };
+    await token.deployed();
+    return { token };
   }
 
   describe("Function 'initialize()'", async () => {
     it("Configures the contract as expected", async () => {
-      const { brlcToken } = await setUpFixture(deployBrlcToken);
-      expect(await brlcToken.owner()).to.equal(deployer.address);
-      expect(await brlcToken.pauser()).to.equal(ethers.constants.AddressZero);
-      expect(await brlcToken.rescuer()).to.equal(ethers.constants.AddressZero);
-      expect(await brlcToken.blacklister()).to.equal(ethers.constants.AddressZero);
-      expect(await brlcToken.decimals()).to.equal(TOKEN_DECIMALS);
-      expect(await brlcToken.bridge()).to.equal(bridge.address);
-      expect(await brlcToken.isIERC20Bridgeable()).to.equal(true);
-      expect(await brlcToken.isBridgeSupported(bridge.address)).to.equal(true);
+      const { token } = await setUpFixture(deployToken);
+      expect(await token.owner()).to.equal(deployer.address);
+      expect(await token.pauser()).to.equal(ethers.constants.AddressZero);
+      expect(await token.rescuer()).to.equal(ethers.constants.AddressZero);
+      expect(await token.blacklister()).to.equal(ethers.constants.AddressZero);
+      expect(await token.decimals()).to.equal(TOKEN_DECIMALS);
+      expect(await token.bridge()).to.equal(bridge.address);
+      expect(await token.isIERC20Bridgeable()).to.equal(true);
+      expect(await token.isBridgeSupported(bridge.address)).to.equal(true);
     });
 
-    it("Is reverted if it is called a second time", async () => {
-      const { brlcToken } = await setUpFixture(deployBrlcToken);
+    it("Is reverted if called for the second time", async () => {
+      const { token } = await setUpFixture(deployToken);
       await expect(
-        brlcToken.initialize(TOKEN_NAME, TOKEN_SYMBOL, bridge.address)
-      ).to.be.revertedWith(REVERT_MESSAGE_IF_CONTRACT_IS_ALREADY_INITIALIZED);
+        token.initialize(TOKEN_NAME, TOKEN_SYMBOL, bridge.address)
+      ).to.be.revertedWith(REVERT_MESSAGE_INITIALIZABLE_CONTRACT_IS_ALREADY_INITIALIZED);
     });
 
-    it("Is reverted for the contract implementation if it is called even for the first time", async () => {
-      const brlcTokenImplementation: Contract = await brlcTokenFactory.deploy();
-      await brlcTokenImplementation.deployed();
-
+    it("Is reverted if the contract implementation is called even for the first time", async () => {
+      const tokenImplementation: Contract = await tokenFactory.deploy();
+      await tokenImplementation.deployed();
       await expect(
-        brlcTokenImplementation.initialize(TOKEN_NAME, TOKEN_SYMBOL, bridge.address)
-      ).to.be.revertedWith(REVERT_MESSAGE_IF_CONTRACT_IS_ALREADY_INITIALIZED);
+        tokenImplementation.initialize(TOKEN_NAME, TOKEN_SYMBOL, bridge.address)
+      ).to.be.revertedWith(REVERT_MESSAGE_INITIALIZABLE_CONTRACT_IS_ALREADY_INITIALIZED);
     });
   });
 
-  describe("Function 'mintForBridging()'", async () => {
-    it("Executes as expected and emits the correct event", async () => {
-      const { brlcToken } = await setUpFixture(deployBrlcToken);
-      await expect(
-        brlcToken.connect(bridge).mintForBridging(user1.address, TOKEN_AMOUNT)
-      ).to.changeTokenBalances(
-        brlcToken,
-        [deployer, bridge, brlcToken, user1],
-        [0, 0, 0, +TOKEN_AMOUNT]
-      ).and.to.emit(
-        brlcToken,
-        EVENT_NAME_MINT_FOR_BRIDGING
-      ).withArgs(
-        user1.address,
-        TOKEN_AMOUNT
-      );
-    });
-
-    it("Is reverted if it is called not by the bridge", async () => {
-      const { brlcToken } = await setUpFixture(deployBrlcToken);
-      await expect(
-        brlcToken.mintForBridging(user1.address, TOKEN_AMOUNT)
-      ).to.be.revertedWithCustomError(brlcToken, REVERT_ERROR_IF_CALLER_IS_NOT_BRIDGE);
-    });
-
-    it("Is reverted if it is called to mint for the zero address", async () => {
-      const { brlcToken } = await setUpFixture(deployBrlcToken);
-      await expect(
-        brlcToken.connect(bridge).mintForBridging(ethers.constants.AddressZero, TOKEN_AMOUNT)
-      ).to.be.revertedWith(REVERT_MESSAGE_IF_MINTING_FOR_ZERO_ADDRESS);
-    });
-
-    it("Is reverted if the token minting amount is zero", async () => {
-      const { brlcToken } = await setUpFixture(deployBrlcToken);
-      await expect(
-        brlcToken.connect(bridge).mintForBridging(user1.address, 0)
-      ).to.be.revertedWithCustomError(brlcToken, REVERT_ERROR_IF_MINT_FOR_BRIDGING_AMOUNT_IS_ZERO);
-    });
-  });
-
-  describe("Function 'burnForBridging()'", async () => {
-    it("Executes as expected and emits the correct event", async () => {
-      const { brlcToken } = await setUpFixture(deployBrlcToken);
-      await proveTx(brlcToken.connect(bridge).mintForBridging(user1.address, TOKEN_AMOUNT));
-
-      await expect(
-        brlcToken.connect(bridge).burnForBridging(user1.address, TOKEN_AMOUNT)
-      ).to.changeTokenBalances(
-        brlcToken,
-        [deployer, bridge, brlcToken, user1],
-        [0, 0, 0, -TOKEN_AMOUNT]
-      ).and.to.emit(
-        brlcToken,
-        EVENT_NAME_BURN_FOR_BRIDGING
-      ).withArgs(
-        user1.address,
-        TOKEN_AMOUNT
-      );
-    });
-
-    it("Is reverted if it is called not by the bridge", async () => {
-      const { brlcToken } = await setUpFixture(deployBrlcToken);
-      await expect(
-        brlcToken.burnForBridging(user1.address, TOKEN_AMOUNT)
-      ).to.be.revertedWithCustomError(brlcToken, REVERT_ERROR_IF_CALLER_IS_NOT_BRIDGE);
-    });
-
-    it("Is reverted if it is called to burn from the zero address", async () => {
-      const { brlcToken } = await setUpFixture(deployBrlcToken);
-      await expect(
-        brlcToken.connect(bridge).burnForBridging(ethers.constants.AddressZero, TOKEN_AMOUNT)
-      ).to.be.revertedWith(REVERT_MESSAGE_IF_BURNING_FROM_ZERO_ADDRESS);
-    });
-
-    it("Is reverted if it is called to burn more tokens than the bridge balance", async () => {
-      const { brlcToken } = await setUpFixture(deployBrlcToken);
-      await expect(
-        brlcToken.connect(bridge).burnForBridging(user1.address, TOKEN_AMOUNT + 1)
-      ).to.be.revertedWith(REVERT_MESSAGE_IF_BURNING_AMOUNT_EXCEEDS_THE_BRIDGE_BALANCE);
-    });
-
-    it("Is reverted if the token burning amount is zero", async () => {
-      const { brlcToken } = await setUpFixture(deployBrlcToken);
-      await expect(
-        brlcToken.connect(bridge).burnForBridging(user1.address, 0)
-      ).to.be.revertedWithCustomError(brlcToken, REVERT_ERROR_IF_BURN_FOR_BRIDGING_AMOUNT_IS_ZERO);
-    });
-  });
-
-  describe("Function 'setBridge()'", async () => {
-    it("Executes as expected and emits the correct event", async () => {
-      const { brlcToken } = await setUpFixture(deployBrlcToken);
-      expect(await brlcToken.bridge()).to.eq(bridge.address);
-
-      await expect(brlcToken.setBridge(newBridge.address))
-        .to.emit(brlcToken, EVENT_NAME_SET_BRIDGE)
-        .withArgs(newBridge.address, bridge.address);
-
-      expect(await brlcToken.bridge()).to.eq(newBridge.address);
-    });
-
-    it("Is reverted if it is called not by the owner", async () => {
-      const { brlcToken } = await setUpFixture(deployBrlcToken);
-      expect(await brlcToken.bridge()).to.eq(bridge.address);
-
-      await expect(brlcToken.connect(user1).setBridge(newBridge.address))
-        .to.be.revertedWith(REVERT_MESSAGE_IF_CALLER_IS_NOT_OWNER);
-    });
-  });
-
-  describe("Function 'approveFreezing()'", async () => {
-    it("Approves freezing and emits the correct event", async () => {
-      const { brlcToken } = await setUpFixture(deployAndConfigureBrlcToken);
-      expect(await brlcToken.freezeApproval(user1.address)).to.eq(false);
-      await expect(brlcToken.connect(user1).approveFreezing())
-        .to.emit(brlcToken, EVENT_NAME_FREEZE_APPROVAL).withArgs(user1.address);
-      expect(await brlcToken.freezeApproval(user1.address)).to.eq(true);
-    });
-
-    it("Is reverted if freezing is already approved", async () => {
-      const { brlcToken } = await setUpFixture(deployAndConfigureBrlcToken);
-      await expect(brlcToken.connect(user1).approveFreezing());
-      await expect(brlcToken.connect(user1).approveFreezing())
-        .to.be.revertedWithCustomError(brlcToken, REVERT_ERROR_IF_FREEZING_ALREADY_APPROVED);
-    });
-
-    it("Is reverted if contract is paused", async () => {
-      const { brlcToken } = await setUpFixture(deployAndConfigureBrlcToken);
-      await proveTx(brlcToken.connect(pauser).pause());
-      await expect(brlcToken.connect(user1).approveFreezing())
-        .to.be.revertedWith(REVERT_MESSAGE_IF_CONTRACT_IS_PAUSED);
-    });
-  });
-
-  describe("Function 'freeze()'", async () => {
-    it("Freezes tokens and emits the correct events", async () => {
-      const { brlcToken } = await setUpFixture(deployAndConfigureBrlcToken);
-      await proveTx(brlcToken.connect(user1).approveFreezing());
-
-      expect(await brlcToken.balanceOf(user1.address)).to.eq(ethers.constants.Zero);
-
-      await expect(brlcToken.connect(blacklister).freeze(user1.address, TOKEN_AMOUNT))
-        .to.emit(brlcToken, EVENT_NAME_FREEZE).withArgs(user1.address, TOKEN_AMOUNT, 0);
-      expect(await brlcToken.frozenBalance(user1.address)).to.eq(TOKEN_AMOUNT);
-
-      await proveTx(brlcToken.connect(bridge).mintForBridging(user1.address, TOKEN_AMOUNT));
-      expect(await brlcToken.balanceOf(user1.address)).to.eq(TOKEN_AMOUNT);
-
-      await expect(brlcToken.connect(blacklister).freeze(user1.address, TOKEN_AMOUNT + 1))
-        .to.emit(brlcToken, EVENT_NAME_FREEZE).withArgs(user1.address, TOKEN_AMOUNT + 1, TOKEN_AMOUNT);
-      expect(await brlcToken.frozenBalance(user1.address)).to.eq(TOKEN_AMOUNT + 1);
-
-      await expect(brlcToken.connect(blacklister).freeze(user1.address, TOKEN_AMOUNT - 2))
-      .to.emit(brlcToken, EVENT_NAME_FREEZE).withArgs(user1.address, TOKEN_AMOUNT - 2, TOKEN_AMOUNT + 1);
-      expect(await brlcToken.frozenBalance(user1.address)).to.eq(TOKEN_AMOUNT - 2);
-    });
-
-    it("Is reverted if freezing is not approved", async () => {
-      const { brlcToken } = await setUpFixture(deployAndConfigureBrlcToken);
-      expect(await brlcToken.freezeApproval(user1.address)).to.eq(false);
-      await expect(brlcToken.connect(blacklister).freeze(user1.address, TOKEN_AMOUNT))
-        .to.be.revertedWithCustomError(brlcToken, REVERT_ERROR_IF_FREEZING_NOT_APPROVED);
-    });
-
-    it("Is reverted if contract is paused", async () => {
-      const { brlcToken } = await setUpFixture(deployAndConfigureBrlcToken);
-      await proveTx(brlcToken.connect(pauser).pause());
-      await expect(brlcToken.connect(blacklister).freeze(user1.address, TOKEN_AMOUNT))
-        .to.be.revertedWith(REVERT_MESSAGE_IF_CONTRACT_IS_PAUSED);
-    });
-
-    it("Is reverted if the caller is not a blacklister", async () => {
-      const { brlcToken } = await setUpFixture(deployAndConfigureBrlcToken);
-      await expect(brlcToken.connect(user1).freeze(user2.address, TOKEN_AMOUNT))
-        .to.be.revertedWithCustomError(brlcToken, REVERT_ERROR_IF_CALLER_IS_NOT_BLACKLISTER);
-    });
-  });
-
-  describe("Function 'transferFrozen()'", async () => {
-    it("Transfers frozen tokens and emits correct events", async () => {
-      const { brlcToken } = await setUpFixture(deployAndConfigureBrlcToken);
-      await proveTx(brlcToken.connect(bridge).mintForBridging(user1.address, TOKEN_AMOUNT));
-      await proveTx(brlcToken.connect(user1).approveFreezing());
-      await proveTx(brlcToken.connect(blacklister).freeze(user1.address, TOKEN_AMOUNT));
-      await expect(brlcToken.connect(blacklister).transferFrozen(user1.address, user2.address, TOKEN_AMOUNT))
-        .to.emit(brlcToken, EVENT_NAME_FREEZE_TRANSFER).withArgs(user1.address, TOKEN_AMOUNT)
-        .to.emit(brlcToken, EVENT_NAME_FREEZE).withArgs(user1.address, TOKEN_AMOUNT, 0)
-        .to.changeTokenBalances(
-          brlcToken,
-          [user1, user2],
-          [-TOKEN_AMOUNT, TOKEN_AMOUNT]
-        );
-    });
-
-    it("Is reverted if the caller is not a blacklister", async () => {
-      const { brlcToken } = await setUpFixture(deployAndConfigureBrlcToken);
-      await proveTx(brlcToken.connect(bridge).mintForBridging(user1.address, TOKEN_AMOUNT));
-      await proveTx(brlcToken.connect(user1).approveFreezing());
-      await expect(brlcToken.connect(user2).transferFrozen(user1.address, user2.address, TOKEN_AMOUNT))
-        .to.be.revertedWithCustomError(brlcToken, REVERT_ERROR_IF_CALLER_IS_NOT_BLACKLISTER);
-    });
-
-    it("Is reverted if the contract is paused", async () => {
-      const { brlcToken } = await setUpFixture(deployAndConfigureBrlcToken);
-      await proveTx(brlcToken.connect(bridge).mintForBridging(user1.address, TOKEN_AMOUNT));
-      await proveTx(brlcToken.connect(user1).approveFreezing());
-      await proveTx(brlcToken.connect(pauser).pause());
-      await expect(brlcToken.connect(blacklister).transferFrozen(user1.address, user2.address, TOKEN_AMOUNT))
-        .to.be.revertedWith(REVERT_MESSAGE_IF_CONTRACT_IS_PAUSED);
-    });
-
-    it("Is reverted if there is a lack of frozen balance", async () => {
-      const { brlcToken } = await setUpFixture(deployAndConfigureBrlcToken);
-      await proveTx(brlcToken.connect(bridge).mintForBridging(user1.address, TOKEN_AMOUNT));
-      await proveTx(brlcToken.connect(user1).approveFreezing());
-      await proveTx(brlcToken.connect(blacklister).freeze(user1.address, TOKEN_AMOUNT));
-      await expect(brlcToken.connect(blacklister).transferFrozen(user1.address, user2.address, TOKEN_AMOUNT + 1))
-        .to.be.revertedWithCustomError(brlcToken, REVERT_ERROR_IF_LACK_OF_FROZEN_BALANCE);
-    });
-
-    it("Is reverted if there is a lack of common balance", async () => {
-      const { brlcToken } = await setUpFixture(deployAndConfigureBrlcToken);
-      await proveTx(brlcToken.connect(bridge).mintForBridging(user1.address, TOKEN_AMOUNT));
-      await proveTx(brlcToken.connect(user1).approveFreezing());
-      await proveTx(brlcToken.connect(blacklister).freeze(user1.address, TOKEN_AMOUNT + 1));
-      await expect(brlcToken.connect(blacklister).transferFrozen(user1.address, user2.address, TOKEN_AMOUNT + 1))
-        .to.be.revertedWith(REVERT_MESSAGE_IF_TRANSFER_AMOUNT_EXCEEDS_BALANCE);
-    });
-  });
-
-  describe("Frozen token scenarios", async () => {
-    it("Tokens above the frozen balance can be transferred successfully", async () => {
-      const { brlcToken } = await setUpFixture(deployAndConfigureBrlcToken);
-      await proveTx(brlcToken.connect(bridge).mintForBridging(user1.address, TOKEN_AMOUNT + 1));
-      await proveTx(brlcToken.connect(user1).approveFreezing());
-      await proveTx(brlcToken.connect(blacklister).freeze(user1.address, TOKEN_AMOUNT));
-      await expect(brlcToken.connect(user1).transfer(user2.address, 1))
-        .to.changeTokenBalances(
-          brlcToken,
-          [user1, user2],
-          [-1, 1]
-        );
-    });
-
-    it("Tokens below the frozen balance cannot be transferred successfully", async () => {
-      const { brlcToken } = await setUpFixture(deployAndConfigureBrlcToken);
-      await proveTx(brlcToken.connect(bridge).mintForBridging(user1.address, TOKEN_AMOUNT + 1));
-      await proveTx(brlcToken.connect(user1).approveFreezing());
-      await proveTx(brlcToken.connect(blacklister).freeze(user1.address, TOKEN_AMOUNT));
-      await expect(brlcToken.connect(user1).transfer(user2.address, 2))
-        .to.be.revertedWithCustomError(brlcToken, REVERT_ERROR_IF_TRANSFER_EXCEEDED_FROZEN_AMOUNT);
+  describe("Function 'isBRLCoin'", async () => {
+    it("Returns true", async () => {
+      const { token } = await setUpFixture(deployToken);
+      expect(await token.isBRLCoin()).to.eq(true);
     });
   });
 });
