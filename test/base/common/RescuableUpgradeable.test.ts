551ee944f6b0e5a8e3affae896b57571d07b0c76
igorsenych-cw <igor.senych@cloudwalk.io>
2023-07-31 15:41:17 +0300

    Improve RescuableUpgradeable contract and its tests

--- test/base/common/RescuableUpgradeable.test.ts
+++ test/base/common/RescuableUpgradeable.test.ts
@@ -14,142 +14,142 @@ async function setUpFixture(func: any) {
 }
 
 describe("Contract 'RescuableUpgradeable'", async () => {
-  const TOKEN_AMOUNT = 123;
+  const TOKEN_AMOUNT = 100;
 
-  const EVENT_NAME_RESCUER_CHANGED = "RescuerChanged";
   const EVENT_NAME_TRANSFER = "Transfer";
+  const EVENT_NAME_RESCUER_CHANGED = "RescuerChanged";
 
-  const REVERT_MESSAGE_IF_CONTRACT_IS_ALREADY_INITIALIZED = "Initializable: contract is already initialized";
-  const REVERT_MESSAGE_IF_CONTRACT_IS_NOT_INITIALIZING = "Initializable: contract is not initializing";
-  const REVERT_MESSAGE_IF_CALLER_IS_NOT_OWNER = "Ownable: caller is not the owner";
+  const REVERT_MESSAGE_INITIALIZABLE_CONTRACT_IS_ALREADY_INITIALIZED = "Initializable: contract is already initialized";
+  const REVERT_MESSAGE_INITIALIZABLE_CONTRACT_IS_NOT_INITIALIZING = "Initializable: contract is not initializing";
+  const REVERT_MESSAGE_OWNABLE_CALLER_IS_NOT_THE_OWNER = "Ownable: caller is not the owner";
 
-  const REVERT_ERROR_IF_CALLER_IS_NOT_RESCUER = "UnauthorizedRescuer";
+  const REVERT_ERROR_UNAUTHORIZED_RESCUER = "UnauthorizedRescuer";
 
-  let rescuableMockFactory: ContractFactory;
-  let tokenMockFactory: ContractFactory;
+  let rescuableFactory: ContractFactory;
+  let tokenFactory: ContractFactory;
 
   let deployer: SignerWithAddress;
   let rescuer: SignerWithAddress;
+  let user: SignerWithAddress;
 
   before(async () => {
-    rescuableMockFactory = await ethers.getContractFactory("RescuableUpgradeableMock");
-    tokenMockFactory = await ethers.getContractFactory("ERC20UpgradeableMock");
-    [deployer, rescuer] = await ethers.getSigners();
+    rescuableFactory = await ethers.getContractFactory("RescuableUpgradeableMock");
+    tokenFactory = await ethers.getContractFactory("ERC20UpgradeableMock");
+    [deployer, rescuer, user] = await ethers.getSigners();
   });
 
-  async function deployRescuableMock(): Promise<{ rescuableMock: Contract }> {
-    const rescuableMock: Contract = await upgrades.deployProxy(rescuableMockFactory);
-    await rescuableMock.deployed();
-    return { rescuableMock };
+  async function deployToken(): Promise<{ token: Contract }> {
+    const token: Contract = await upgrades.deployProxy(tokenFactory, ["ERC20 Test", "TEST"]);
+    await token.deployed();
+    return { token };
   }
 
-  async function deployTokenMock(): Promise<{ tokenMock: Contract }> {
-    const tokenMock: Contract = await upgrades.deployProxy(tokenMockFactory, ["ERC20 Test", "TEST"]);
-    await tokenMock.deployed();
-    return { tokenMock };
+  async function deployRescuable(): Promise<{ rescuable: Contract }> {
+    const rescuable: Contract = await upgrades.deployProxy(rescuableFactory);
+    await rescuable.deployed();
+    return { rescuable };
   }
 
-  async function deployAndConfigureAllContracts(): Promise<{ rescuableMock: Contract, tokenMock: Contract }> {
-    const { rescuableMock } = await deployRescuableMock();
-    const { tokenMock } = await deployTokenMock();
-
-    await proveTx(tokenMock.mint(rescuableMock.address, TOKEN_AMOUNT));
-    await proveTx(rescuableMock.setRescuer(rescuer.address));
-
-    return {
-      rescuableMock,
-      tokenMock
-    };
+  async function deployAndConfigure(): Promise<{ rescuable: Contract, token: Contract }> {
+    const { rescuable } = await deployRescuable();
+    const { token } = await deployToken();
+    await proveTx(token.mint(rescuable.address, TOKEN_AMOUNT));
+    await proveTx(rescuable.setRescuer(rescuer.address));
+    return { rescuable, token };
   }
 
   describe("Function 'initialize()'", async () => {
-    it("The external initializer configures the contract as expected", async () => {
-      const { rescuableMock } = await setUpFixture(deployRescuableMock);
-
-      expect(await rescuableMock.owner()).to.equal(deployer.address);
-      expect(await rescuableMock.rescuer()).to.equal(ethers.constants.AddressZero);
+    it("Configures the contract as expected", async () => {
+      const { rescuable } = await setUpFixture(deployRescuable);
+      expect(await rescuable.owner()).to.equal(deployer.address);
+      expect(await rescuable.rescuer()).to.equal(ethers.constants.AddressZero);
     });
 
-    it("The external initializer is reverted if it is called a second time", async () => {
-      const { rescuableMock } = await setUpFixture(deployRescuableMock);
+    it("Is reverted if called for the second time", async () => {
+      const { rescuable } = await setUpFixture(deployRescuable);
       await expect(
-        rescuableMock.initialize()
-      ).to.be.revertedWith(REVERT_MESSAGE_IF_CONTRACT_IS_ALREADY_INITIALIZED);
+        rescuable.initialize()
+      ).to.be.revertedWith(REVERT_MESSAGE_INITIALIZABLE_CONTRACT_IS_ALREADY_INITIALIZED);
     });
 
-    it("The internal initializer is reverted if it is called outside the init process", async () => {
-      const { rescuableMock } = await setUpFixture(deployRescuableMock);
+    it("Is reverted if the implementation contract is called even for the first time", async () => {
+      const rescuableImplementation: Contract = await rescuableFactory.deploy();
+      await rescuableImplementation.deployed();
       await expect(
-        rescuableMock.call_parent_initialize()
-      ).to.be.revertedWith(REVERT_MESSAGE_IF_CONTRACT_IS_NOT_INITIALIZING);
+        rescuableImplementation.initialize()
+      ).to.be.revertedWith(REVERT_MESSAGE_INITIALIZABLE_CONTRACT_IS_ALREADY_INITIALIZED);
     });
 
-    it("The internal unchained initializer is reverted if it is called outside the init process", async () => {
-      const { rescuableMock } = await setUpFixture(deployRescuableMock);
+    it("Is reverted if the internal initializer is called outside of the init process", async () => {
+      const { rescuable } = await setUpFixture(deployRescuable);
       await expect(
-        rescuableMock.call_parent_initialize_unchained()
-      ).to.be.revertedWith(REVERT_MESSAGE_IF_CONTRACT_IS_NOT_INITIALIZING);
+        rescuable.call_parent_initialize()
+      ).to.be.revertedWith(REVERT_MESSAGE_INITIALIZABLE_CONTRACT_IS_NOT_INITIALIZING);
+    });
+
+    it("Is reverted if the internal unchained initializer is called outside of the init process", async () => {
+      const { rescuable } = await setUpFixture(deployRescuable);
+      await expect(
+        rescuable.call_parent_initialize_unchained()
+      ).to.be.revertedWith(REVERT_MESSAGE_INITIALIZABLE_CONTRACT_IS_NOT_INITIALIZING);
     });
   });
 
   describe("Function 'setRescuer()'", async () => {
-    it("Executes successfully and emits the correct event if it is called by the owner", async () => {
-      const { rescuableMock } = await setUpFixture(deployRescuableMock);
+    it("Executes successfully and emits the correct event", async () => {
+      const { rescuable } = await setUpFixture(deployRescuable);
       await expect(
-        rescuableMock.setRescuer(rescuer.address)
+        rescuable.connect(deployer).setRescuer(rescuer.address)
       ).to.emit(
-        rescuableMock,
+        rescuable,
         EVENT_NAME_RESCUER_CHANGED
       ).withArgs(rescuer.address);
-      expect(await rescuableMock.rescuer()).to.equal(rescuer.address);
-
-      // The second call with the same argument should not emit an event
+      expect(await rescuable.rescuer()).to.equal(rescuer.address);
       await expect(
-        rescuableMock.setRescuer(rescuer.address)
-      ).not.to.emit(rescuableMock, EVENT_NAME_RESCUER_CHANGED);
+        rescuable.connect(deployer).setRescuer(rescuer.address)
+      ).not.to.emit(rescuable, EVENT_NAME_RESCUER_CHANGED);
     });
 
-    it("Is reverted if it is called not by the owner", async () => {
-      const { rescuableMock } = await setUpFixture(deployRescuableMock);
+    it("Is reverted if called not by the owner", async () => {
+      const { rescuable } = await setUpFixture(deployRescuable);
       await expect(
-        rescuableMock.connect(rescuer).setRescuer(rescuer.address)
-      ).to.be.revertedWith(REVERT_MESSAGE_IF_CALLER_IS_NOT_OWNER);
+        rescuable.connect(rescuer).setRescuer(rescuer.address)
+      ).to.be.revertedWith(REVERT_MESSAGE_OWNABLE_CALLER_IS_NOT_THE_OWNER);
     });
   });
 
   describe("Function 'rescueERC20()'", async () => {
     it("Executes as expected and emits the correct event", async () => {
-      const { rescuableMock, tokenMock } = await setUpFixture(deployAndConfigureAllContracts);
-
+      const { rescuable, token } = await setUpFixture(deployAndConfigure);
       await expect(
-        rescuableMock.connect(rescuer).rescueERC20(
-          tokenMock.address,
+        rescuable.connect(rescuer).rescueERC20(
+          token.address,
           deployer.address,
           TOKEN_AMOUNT
         )
       ).to.changeTokenBalances(
-        tokenMock,
-        [rescuableMock, deployer, rescuer],
+        token,
+        [rescuable, deployer, rescuer],
         [-TOKEN_AMOUNT, +TOKEN_AMOUNT, 0]
       ).and.to.emit(
-        tokenMock,
+        token,
         EVENT_NAME_TRANSFER
       ).withArgs(
-        rescuableMock.address,
+        rescuable.address,
         deployer.address,
         TOKEN_AMOUNT
       );
     });
 
-    it("Is reverted if it is called not by the rescuer", async () => {
-      const { rescuableMock, tokenMock } = await setUpFixture(deployAndConfigureAllContracts);
+    it("Is reverted if called not by the rescuer", async () => {
+      const { rescuable, token } = await setUpFixture(deployAndConfigure);
       await expect(
-        rescuableMock.rescueERC20(
-          tokenMock.address,
+        rescuable.connect(user).rescueERC20(
+          token.address,
           deployer.address,
           TOKEN_AMOUNT
         )
-      ).to.be.revertedWithCustomError(rescuableMock, REVERT_ERROR_IF_CALLER_IS_NOT_RESCUER);
+      ).to.be.revertedWithCustomError(rescuable, REVERT_ERROR_UNAUTHORIZED_RESCUER);
     });
   });
 });
