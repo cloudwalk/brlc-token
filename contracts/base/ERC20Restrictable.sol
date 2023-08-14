8126dc9bffae634eccfa9679656a2c3cdaea3a09
igorsenych-cw <igor.senych@cloudwalk.io>
2023-08-14 13:32:18 +0300

    Create ERC20Restrictable contract draft with storage gap

---
+++ contracts/base/ERC20Restrictable.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.16;
+
+import { ERC20Base } from "./ERC20Base.sol";
+
+/**
+ * @title ERC20Restrictable contract
+ * @author CloudWalk Inc.
+ */
+abstract contract ERC20Restrictable is ERC20Base {
+    /**
+     * @dev This empty reserved space is put in place to allow future versions
+     * to add new variables without shifting down storage in the inheritance chain.
+     */
+    uint256[50] private __gap;
+}
