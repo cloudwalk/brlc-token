2de0610ff1e8a3248b6d139e39d3e8bf7b9fcbb3
Igor Senych <igor.senych@cloudwalk.io>
2024-01-05 20:24:24 +0200

    Install and configure ESLint/Prettier

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
+import "hardhat-contract-sizer";
 
 const config: HardhatUserConfig = {
   solidity: {
