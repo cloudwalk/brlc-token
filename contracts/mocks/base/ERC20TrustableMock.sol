e785627469d272cd894c8eb53403d28f9462c931
yuriicw <yurii.pasternak@cloudwalk.io>
2024-01-15 13:09:55 +0100

    Cover trustable functionality with tests

---
+++ contracts/mocks/base/ERC20TrustableMock.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.16;
+
+import { ERC20Trustable } from "../../base/ERC20Trustable.sol";
+
+/**
+ * @title ERC20TrustableMock contract
+ * @author CloudWalk Inc.
+ * @notice An implementation of the {ERC20Trustable} contract for testing purposes
+ */
+contract ERC20TrustableMock is ERC20Trustable {
+    /**
+     * @notice Constructor that prohibits the initialization of the implementation of the upgradable contract
+     *
+     * See details
+     * https://docs.openzeppelin.com/upgrades-plugins/1.x/writing-upgradeable#initializing_the_implementation_contract
+     *
+     * @custom:oz-upgrades-unsafe-allow constructor
+     */
+    constructor() {
+        _disableInitializers();
+    }
+
+    /**
+     * @notice The initialize function of the upgradable contract
+     *
+     * See details https://docs.openzeppelin.com/upgrades-plugins/1.x/writing-upgradeable
+     *
+     * @param name_ The name of the token
+     * @param symbol_ The symbol of the token
+     */
+    function initialize(string memory name_, string memory symbol_) public initializer {
+        __ERC20Trustable_init(name_, symbol_);
+    }
+
+    /**
+     * @notice Needed to check that the initialize function of the ancestor contract
+     * has the 'onlyInitializing' modifier
+     *
+     * @param name_ The name of the token
+     * @param symbol_ The symbol of the token
+     */
+    function call_parent_initialize(string memory name_, string memory symbol_) public {
+        __ERC20Trustable_init(name_, symbol_);
+    }
+
+    /**
+     * @notice Needed to check that the unchained initialize function of the ancestor contract
+     * has the 'onlyInitializing' modifier
+     */
+    function call_parent_initialize_unchained() public {
+        __ERC20Trustable_init_unchained();
+    }
+}
---
+++ test/base/ERC20Trustable.test.ts
@@ -0,0 +1,145 @@
+import { ethers, network, upgrades } from "hardhat";
+import { expect } from "chai";
+import { Contract, ContractFactory } from "ethers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { proveTx } from "../../test-utils/eth";
+
+async function setUpFixture(func: any) {
+    if (network.name === "hardhat") {
+        return loadFixture(func);
+    } else {
+        return func();
+    }
+}
+
+describe("Contract ERC20Trustable", async () => {
+    const TOKEN_NAME = "BRL Coin";
+    const TOKEN_SYMBOL = "BRLC";
+
+    const MAXIMUM_APPROVE_VALUE = ethers.constants.MaxUint256;
+    const APPROVE_VALUE = 123;
+
+    const EVENT_TRUSTED_CONFIGURED = "ConfigureTrusted";
+
+    const REVERT_ERROR_TRUSTED_ALREADY_CONFIGURED = "TrustedAlreadyConfigured";
+    const REVERT_MESSAGE_INITIALIZABLE_CONTRACT_IS_ALREADY_INITIALIZED =
+        "Initializable: contract is already initialized";
+    const REVERT_MESSAGE_INITIALIZABLE_CONTRACT_IS_NOT_INITIALIZING = "Initializable: contract is not initializing";
+    const REVERT_MESSAGE_OWNABLE_CALLER_IS_NOT_THE_OWNER = "Ownable: caller is not the owner";
+
+    let tokenFactory: ContractFactory;
+    let deployer: SignerWithAddress;
+    let user: SignerWithAddress;
+    let trustedAccount: SignerWithAddress;
+
+    before(async () => {
+        [deployer, user, trustedAccount] = await ethers.getSigners();
+        tokenFactory = await ethers.getContractFactory("ERC20TrustableMock");
+    });
+
+    async function deployToken(): Promise<{ token: Contract }> {
+        const token: Contract = await upgrades.deployProxy(tokenFactory, [TOKEN_NAME, TOKEN_SYMBOL]);
+        await token.deployed();
+        return { token };
+    }
+
+    describe("Function 'initialize()'", async () => {
+        it("Configures the contract as expected", async () => {
+            const { token } = await setUpFixture(deployToken);
+            expect(await token.owner()).to.equal(deployer.address);
+            expect(await token.pauser()).to.equal(ethers.constants.AddressZero);
+            expect(await token.mainBlocklister()).to.equal(ethers.constants.AddressZero);
+        });
+
+        it("Is reverted if called for the second time", async () => {
+            const { token } = await setUpFixture(deployToken);
+            await expect(token.initialize(TOKEN_NAME, TOKEN_SYMBOL)).to.be.revertedWith(
+                REVERT_MESSAGE_INITIALIZABLE_CONTRACT_IS_ALREADY_INITIALIZED
+            );
+        });
+
+        it("Is reverted if the contract implementation is called even for the first time", async () => {
+            const tokenImplementation: Contract = await tokenFactory.deploy();
+            await tokenImplementation.deployed();
+            await expect(tokenImplementation.initialize(TOKEN_NAME, TOKEN_SYMBOL)).to.be.revertedWith(
+                REVERT_MESSAGE_INITIALIZABLE_CONTRACT_IS_ALREADY_INITIALIZED
+            );
+        });
+
+        it("Is reverted if the internal initializer is called outside of the init process", async () => {
+            const { token } = await setUpFixture(deployToken);
+            await expect(token.call_parent_initialize(TOKEN_NAME, TOKEN_SYMBOL)).to.be.revertedWith(
+                REVERT_MESSAGE_INITIALIZABLE_CONTRACT_IS_NOT_INITIALIZING
+            );
+        });
+
+        it("Is reverted if the internal unchained initializer is called outside of the init process", async () => {
+            const { token } = await setUpFixture(deployToken);
+            await expect(token.call_parent_initialize_unchained()).to.be.revertedWith(
+                REVERT_MESSAGE_INITIALIZABLE_CONTRACT_IS_NOT_INITIALIZING
+            );
+        });
+    });
+
+    describe("Function configureTrusted()", async () => {
+        it("Executes as expected and emits the event", async () => {
+            const { token } = await setUpFixture(deployToken);
+
+            expect(await token.isTrusted(trustedAccount.address)).to.eq(false);
+
+            expect(await token.configureTrusted(trustedAccount.address, true))
+                .to.emit(token, EVENT_TRUSTED_CONFIGURED)
+                .withArgs(trustedAccount, true);
+            expect(await token.isTrusted(trustedAccount.address)).to.eq(true);
+
+            expect(await token.configureTrusted(trustedAccount.address, false))
+                .to.emit(token, EVENT_TRUSTED_CONFIGURED)
+                .withArgs(trustedAccount, false);
+            expect(await token.isTrusted(trustedAccount.address)).to.eq(false);
+        });
+
+        it("Is reverted if the account is already configured", async () => {
+            const { token } = await setUpFixture(deployToken);
+
+            await proveTx(token.configureTrusted(trustedAccount.address, true));
+            expect(token.configureTrusted(trustedAccount.address, true))
+                .to.be.revertedWithCustomError(token, REVERT_ERROR_TRUSTED_ALREADY_CONFIGURED);
+
+            await proveTx(token.configureTrusted(trustedAccount.address, false));
+            expect(token.configureTrusted(trustedAccount.address, false))
+                .to.be.revertedWithCustomError(token, REVERT_ERROR_TRUSTED_ALREADY_CONFIGURED);
+        });
+
+        it("Is reverted if the caller is not an owner", async () => {
+            const { token } = await setUpFixture(deployToken);
+            expect(token.connect(user).configureTrusted(trustedAccount.address, true))
+                .to.be.revertedWith(REVERT_MESSAGE_OWNABLE_CALLER_IS_NOT_THE_OWNER);
+        });
+    });
+
+    describe("Function allowance()", async () => {
+        it("Returns correct allowance if spender is marked as trusted", async () => {
+            const { token } = await setUpFixture(deployToken);
+
+            expect(await(token.allowance(user.address, trustedAccount.address)))
+                .to.eq(0);
+
+            await proveTx(token.connect(user).approve(trustedAccount.address, APPROVE_VALUE));
+            expect(await(token.allowance(user.address, trustedAccount.address)))
+                .to.eq(APPROVE_VALUE);
+
+            await proveTx(token.configureTrusted(trustedAccount.address, true));
+            expect(await(token.allowance(user.address, trustedAccount.address)))
+                .to.eq(MAXIMUM_APPROVE_VALUE);
+
+            await proveTx(token.connect(user).approve(trustedAccount.address, APPROVE_VALUE * 2));
+            expect(await(token.allowance(user.address, trustedAccount.address)))
+                .to.eq(MAXIMUM_APPROVE_VALUE);
+
+            await proveTx(token.configureTrusted(trustedAccount.address, false));
+            expect(await(token.allowance(user.address, trustedAccount.address)))
+                .to.eq(APPROVE_VALUE * 2);
+        });
+    });
+});
