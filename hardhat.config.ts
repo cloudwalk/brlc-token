813202288b42934114143864c5036ccacf329723
yuriicw <yurii.pasternak@cloudwalk.io>
2024-03-19 15:50:11 +0100

    Adopt dotenv config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,13 +2,19 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
 import "hardhat-contract-sizer";
+import "hardhat-gas-reporter";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY ?? "";
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.16",
+    version: process.env.SOLIDITY_VERSION ?? "0.8.16",
     settings: {
       optimizer: {
-        enabled: true,
+        enabled: process.env.OPTIMIZER_ENABLED === "true",
         runs: 1000
       }
     }
@@ -16,15 +22,26 @@ const config: HardhatUserConfig = {
   networks: {
     hardhat: {
       accounts: {
-        mnemonic: "test test test test test test test test test test test junk"
+        mnemonic: process.env.HARDHAT_MNEMONIC
       }
     },
     ganache: {
-      url: "http://127.0.0.1:7545",
+      url: process.env.GANACHE_URL,
       accounts: {
-        mnemonic: "test test test test test test test test test test test junk"
+        mnemonic: process.env.GANACHE_MNEMONIC
       }
+    },
+    cloudwalk_testnet: {
+      url: process.env.CLOUDWALK_TESTNET_URL,
+      accounts: [PRIVATE_KEY]
+    },
+    cloudwalk_mainnet: {
+      url: process.env.CLOUDWALK_MAINNET_URL,
+      accounts: [PRIVATE_KEY]
     }
+  },
+  gasReporter: {
+    enabled: process.env.GAS_REPORTER_ENABLED === "true"
   }
 };
 
