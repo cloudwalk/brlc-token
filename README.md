d667327b1b4293efa1641f1664d65432ff53f822
Yurii <113507287+yuriicw@users.noreply.github.com>
2024-03-26 06:27:37 +0100

    Enable Hardhat configuration using environment variables (#71)

--- README.md
+++ README.md
@@ -11,6 +11,19 @@
 This repository contains [BRLC](https://infinitepay.io/brlc) token smart contracts.</br>
 [BRLC](https://infinitepay.io/brlc) is a stablecoin created and issued by [Infinitepay](https://infinitepay.io).
 
+## Project Setup
+1. Clone the repo.
+2. Create the `.env` file based on the `.env.example` one:
+    * Windows:
+    ```sh
+    copy .env.example .env
+    ```
+    * MacOS/Linux:
+    ```sh
+    cp .env.example .env
+    ```
+3. Update settings in the newly created `.env` file if needed (e.g. another solidity version, number of optimization runs, private keys (PK) for networks, network RPC URLs, etc.).
+
 ## Build and test
 
 ```sh
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,29 +2,52 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
 import "hardhat-contract-sizer";
+import "hardhat-gas-reporter";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.16",
+    version: process.env.SOLIDITY_VERSION ?? "",
     settings: {
       optimizer: {
-        enabled: true,
-        runs: 1000
+        enabled: process.env.OPTIMIZER_ENABLED === "true",
+        runs: Number(process.env.OPTIMIZER_RUNS)
       }
     }
   },
   networks: {
     hardhat: {
       accounts: {
-        mnemonic: "test test test test test test test test test test test junk"
+        mnemonic: process.env.HARDHAT_MNEMONIC
       }
     },
     ganache: {
-      url: "http://127.0.0.1:7545",
+      url: process.env.GANACHE_RPC,
       accounts: {
-        mnemonic: "test test test test test test test test test test test junk"
+        mnemonic: process.env.GANACHE_MNEMONIC
       }
+    },
+    cw_testnet: {
+      url: process.env.CW_TESTNET_RPC,
+      accounts: process.env.CW_TESTNET_PK
+        ? [process.env.CW_TESTNET_PK]
+        : {
+          mnemonic: process.env.CW_TESTNET_MNEMONIC || ""
+        }
+    },
+    cw_mainnet: {
+      url: process.env.CW_MAINNET_RPC,
+      accounts: process.env.CW_MAINNET_PK
+        ? [process.env.CW_MAINNET_PK]
+        : {
+          mnemonic: process.env.CW_MAINNET_MNEMONIC || ""
+        }
     }
+  },
+  gasReporter: {
+    enabled: process.env.GAS_REPORTER_ENABLED === "true"
   }
 };
 
