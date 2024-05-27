80bdba3c2e92ed0e81076347e2ec2b4cf7cdb1b8
Igor Senych <igor.senych@cloudwalk.io>
2024-05-27 18:21:42 +0300

    test: smart contract tests improvement (#78)
    * test: use a simpler function to get the address of a contract
    * test: introduce special functions to work with block timestamps
    * test: import transaction-related classes from package "ethers"

--- test-utils/eth.ts
+++ test-utils/eth.ts
@@ -1,12 +1,51 @@
-import { BaseContract, Contract } from "ethers";
+import { ethers, network } from "hardhat";
+import { BaseContract, BlockTag, Contract, TransactionReceipt, TransactionResponse } from "ethers";
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
-import { TransactionReceipt, TransactionResponse } from "@ethersproject/abstract-provider";
+import { time } from "@nomicfoundation/hardhat-network-helpers";
 
 export async function proveTx(txResponsePromise: Promise<TransactionResponse>): Promise<TransactionReceipt> {
-  const txReceipt = await txResponsePromise;
-  return txReceipt.wait();
+  const txResponse = await txResponsePromise;
+  const txReceipt = await txResponse.wait();
+  if (!txReceipt) {
+    throw new Error("The transaction receipt is empty");
+  }
+  return txReceipt;
 }
 
 export function connect(contract: BaseContract, signer: HardhatEthersSigner): Contract {
   return contract.connect(signer) as Contract;
+}
+
+export function getAddress(contract: Contract): string {
+  const address = contract.target;
+  if (typeof address !== "string" || address.length != 42 || !address.startsWith("0x")) {
+    throw new Error("The '.target' field of the contract is not an address string");
+  }
+  return address;
+}
+
+export async function getTxTimestamp(tx: Promise<TransactionResponse>): Promise<number> {
+  const receipt = await proveTx(tx);
+  const block = await ethers.provider.getBlock(receipt.blockNumber);
+  return Number(block?.timestamp ?? 0);
+}
+
+export async function getBlockTimestamp(blockTag: BlockTag): Promise<number> {
+  const block = await ethers.provider.getBlock(blockTag);
+  return block?.timestamp ?? 0;
+}
+
+export async function getLatestBlockTimestamp(): Promise<number> {
+  return getBlockTimestamp("latest");
+}
+
+export async function increaseBlockTimestampTo(target: number) {
+  if (network.name === "hardhat") {
+    await time.increaseTo(target);
+  } else if (network.name === "stratus") {
+    await ethers.provider.send("evm_setNextBlockTimestamp", [target]);
+    await ethers.provider.send("evm_mine", []);
+  } else {
+    throw new Error(`Setting block timestamp for the current blockchain is not supported: ${network.name}`);
+  }
 }
