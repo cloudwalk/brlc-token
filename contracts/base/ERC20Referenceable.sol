2c0c62c7211d1850e5771f0238f6109a13616cec
Igor Senych <igor.senych@cloudwalk.io>
2024-06-04 16:47:29 +0300

    feat: review

--- contracts/base/ERC20Referenceable.sol
+++ contracts/base/ERC20Referenceable.sol
@@ -10,59 +10,23 @@ import { ERC20Base } from "./ERC20Base.sol";
  * @notice The ERC20 token implementation that supports referenced operations
  */
 abstract contract ERC20Referenceable is ERC20Base {
-    // give some more data for reference to perform additional action before/after transfer
-    enum ReferenceFlag {
-        Zero,
-        One,
-        Two,
-        Three
-        // ...
-    }
-
     struct Reference {
         address receiver;
-        ReferenceFlag flag;
+        uint256 amount;
     }
 
     // user => id => amount
-    mapping(address => mapping(bytes32 => uint256)) internal _accountReferences;
+    mapping(bytes32 => Reference) internal _accountReferences;
+
     // user => total amount on all references
     mapping(address => uint256) internal _totalReferencedFromAccount;
-    // id => total amount on all users
-    mapping(bytes32 => uint256) internal _totalReferencedToId;
-    // id => reference config
-    mapping(bytes32 => Reference) internal _references;
-    // user => assigned references
-    mapping(address => bytes32[]) internal _referencedId;
 
     event AccountReferenceIncrease(bytes32 id, address account, uint256 amount);
     event AccountReferenceDecrease(bytes32 id, address account, uint256 amount);
-    event ReferenceConfigured(bytes32 id, address receiver, ReferenceFlag flag);
-    event ReferencesAssigned(address account, bytes32[] ids);
 
     error InvalidAmount();
     error UnsupportedReferenceId();
     error TransferExceededReferencedAmount();
-
-    function configureReference(bytes32 id, address receiver, ReferenceFlag flag) external {
-        if (receiver == address(0)) {
-            revert ZeroAddress();
-        }
-
-        _references[id] = Reference({
-            receiver : receiver,
-            flag : flag
-        });
-
-        emit ReferenceConfigured(id, receiver, flag);
-    }
-
-    function assignReferences(address account, bytes32[] memory ids) external {
-        // todo check references
-        _referencedId[account] = ids;
-
-        emit ReferencesAssigned(account, ids);
-    }
 
     function increaseAccountReference(bytes32 id, address account, uint256 amount) external {
         if (account == address(0)) {
@@ -71,13 +35,9 @@ abstract contract ERC20Referenceable is ERC20Base {
         if (amount == 0) {
             revert ZeroAmount();
         }
-        if (_references[id].receiver == address(0)) {
-            revert UnsupportedReferenceId();
-        }
 
         _accountReferences[account][id] += amount;
         _totalReferencedFromAccount[account] += amount;
-        _totalReferencedToId[id] += amount;
 
         emit AccountReferenceIncrease(id, account, amount);
     }
@@ -94,7 +54,7 @@ abstract contract ERC20Referenceable is ERC20Base {
         }
         _accountReferences[account][id] -= amount;
         _totalReferencedFromAccount[account] -= amount;
-        _totalReferencedToId[id] -= amount;
+
         emit AccountReferenceDecrease(id, account, amount);
     }
 
@@ -104,14 +64,6 @@ abstract contract ERC20Referenceable is ERC20Base {
 
     function balanceOfReferenced(address account) public view returns (uint256) {
         return _totalReferencedFromAccount[account];
-    }
-
-    function getTotalReferencedToId(bytes32 id) external view returns (uint256) {
-        return _totalReferencedToId[id];
-    }
-
-    function getReference(bytes32 id) external view returns (Reference memory) {
-        return _references[id];
     }
 
     function _afterTokenTransfer(address from, address to, uint256 amount) internal virtual override {
