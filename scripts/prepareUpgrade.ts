af885e762b0cc7edf98c251bd8ebdab722d16809
ihoroleksiienko <ihor.oleksiienko@cloudwalk.io>
2024-11-08 15:37:18 +0200

    chore: update scripts

--- scripts/prepareUpgrade.ts
+++ scripts/prepareUpgrade.ts
@@ -4,12 +4,11 @@ async function main() {
   const CONTRACT_NAME: string = ""; // TBD: Enter contract name
   const PROXY_ADDRESS: string = ""; // TBD: Enter proxy address
 
-  // Upgrade options:
-  // unsafeAllowRenames: true
-  // unsafeSkipStorageCheck: true
-
   const factory = await ethers.getContractFactory(CONTRACT_NAME);
-  const response = await upgrades.prepareUpgrade(PROXY_ADDRESS, factory);
+  const response = await upgrades.prepareUpgrade(PROXY_ADDRESS, factory, {
+    unsafeAllowRenames: false,
+    unsafeSkipStorageCheck: false
+  });
 
   console.log("Upgrade prepared:", response);
 }
