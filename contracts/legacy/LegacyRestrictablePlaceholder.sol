22f2c87dbace106e4e67638492b3e246af7732ba
Igor Senych <igor.senych@cloudwalk.io>
2025-04-08 16:32:06 +0300

    fix: return `__gap` to `LegacyRestrictablePlaceholder` contract storage

--- contracts/legacy/LegacyRestrictablePlaceholder.sol
+++ contracts/legacy/LegacyRestrictablePlaceholder.sol
@@ -75,4 +75,10 @@ abstract contract LegacyRestrictablePlaceholder {
 
     /// @notice Thrown when the transfer amount exceeds the restricted balance
     error TransferExceededRestrictedAmount();
+
+    /**
+     * @dev This empty reserved space is put in place to allow future versions
+     * to add new variables without shifting down storage in the inheritance chain
+     */
+    uint256[47] private __gap;
 }
