fdecde1ea9724b7698d5345f75cc230b5f617f2d
Evgenii Zaitsev <evgenii@cloudwalk.io>
2025-04-15 17:27:50 +0700

    test: make a stub file for BRLCTokenBridgeable tests

---
+++ test/BRLCTokenBridgeable.test.ts
@@ -0,0 +1,10 @@
+import { expect } from "chai";
+
+// NOTE! The corresponding smart contract for which the tests from this file were used no longer exists.
+// But this file is left for compatibility with existing E2E blockchain tests.
+// After their modification, this file can be deleted.
+describe("Contract 'BRLCTokenBridgeable'", async () => {
+  it("Does not exist anymore", async () => {
+    expect(0).to.equal(0);
+  });
+});
